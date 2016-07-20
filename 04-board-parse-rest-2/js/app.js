2d01a009f594cc01ae4cdb59728a51c8e32fb2bb
halokin <halokin@gmail.com>
2016-07-20 16:34:30 +0200

    front end message board parse REST web app on heroku copy folder

---
+++ 04-board-parse-rest-2/js/app.js
@@ -0,0 +1,76 @@
+var parseAppID = 'Zc36GIp6WyzKIB9HvqRBEGnIeMO0X21rDbVwGPvp'
+var parseRestKey = '1LKZVd4KG6PFpldvCha5RDY8Z5EZeJhy2Bl4vgJ3'
+
+// Azat's app, change these values to your own keys
+parseAppID = 'Zc36GIp6WyzKIB9HvqRBEGnIeMO0X21rDbVwGPvp'
+parseRestKey = '1LKZVd4KG6PFpldvCha5RDY8Z5EZeJhy2Bl4vgJ3'
+
+$(document).ready(function(){
+	getMessages()
+	$('#send').click(function(){
+		var $sendButton = $(this)
+		$sendButton.html('<img src="img/spinner.gif" width="20"/>')
+		var username = $('input[name=username]').val()
+		var message = $('input[name=message]').val()
+		$.ajax({
+			url: ' https://api.parse.com/1/classes/MessageBoard',
+			headers: {
+				'X-Parse-Application-Id': parseAppID,
+				'X-Parse-REST-API-Key': parseRestKey
+			},
+			contentType: 'application/json',
+			dataType: 'json',
+			processData: false,
+			data: JSON.stringify({
+				'username': username,
+				'message': message
+			}),
+			type: 'POST',
+			success: function() {
+				console.log('sent')
+				getMessages()
+				$sendButton.html('SEND')
+			},
+			error: function() {
+				console.log('error')
+				$sendButton.html('SEND')
+			}
+		})
+
+	})
+})
+function getMessages() {
+	$.ajax({
+		url: ' https://api.parse.com/1/classes/MessageBoard?limit=1000',
+		headers: {
+			'X-Parse-Application-Id': 'Zc36GIp6WyzKIB9HvqRBEGnIeMO0X21rDbVwGPvp',
+			'X-Parse-REST-API-Key': '1LKZVd4KG6PFpldvCha5RDY8Z5EZeJhy2Bl4vgJ3'
+		},
+		contentType: 'application/json',
+		dataType: 'json',
+		type: 'GET',
+		success: function(data) {
+			console.log('get')
+			updateView(data)
+		},
+		error: function() {
+			console.log('error')
+		}
+	})
+}
+
+function updateView(messages) {
+	// messages.results = messages.results.reverse()
+	var table = $('.table tbody')
+	table.html('')
+	$.each(messages.results, function (index, value) {
+		var trEl = ('<tr><td>'
+		  + value.username
+			+ '</td><td>'
+			+ value.message
+			+ '</td></tr>')
+		table.append(trEl)
+	})
+
+	console.log(messages)
+}
